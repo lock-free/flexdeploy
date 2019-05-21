2bb72debd201c9f5ae5f981fef529eba9ea444ea
loveKino <842914439@qq.com>
2019-05-21 14:53:53 +0800

    fix config modify problem

--- src/dockerComposeYml.js
+++ src/dockerComposeYml.js
@@ -36,7 +36,7 @@ const generateDCY = async ({
     name = name || `docker-compose-${envName}.yml`;
 
     const tarPath = path.join(dir, name);
-    const txt = parseTpl(tpl, _.assign(config, {
+    const txt = parseTpl(tpl, _.assign({}, config, {
       env: envName,
       project: config.project
     }, vars));
